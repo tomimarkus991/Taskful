054fd2f486788c54f5a6a1356cda2d83ca4d78ca
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-05-15 16:31:00 +0300

    Add authtentication basics

--- client/src/App.js
+++ client/src/App.js
@@ -1,28 +1,31 @@
-import React, { Fragment } from 'react';
-import './App.css';
-import Navbar from './components/layout/Navbar';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Home from './components/pages/Home';
-import About from './components/pages/About';
+import React, { Fragment } from "react";
+import "./App.css";
+import Navbar from "./components/layout/Navbar";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
 
-import TaskState from './context/task/TaskState';
+import TaskState from "./context/task/TaskState";
+import AuthState from "./context/auth/AuthState";
 
 const App = () => {
   return (
-    <TaskState>
-      <Router>
-        <Fragment>
-          <Navbar />
-          <div className="container">
-            <Switch>
-              <Route exact path='/' component={Home} />
-              <Route exact path='/about' component={About} />
-            </Switch>
-          </div>
-        </Fragment>
-      </Router>
-    </TaskState>
+    <AuthState>
+      <TaskState>
+        <Router>
+          <Fragment>
+            <Navbar />
+            <div className="container">
+              <Switch>
+                <Route exact path="/" component={Home} />
+                <Route exact path="/about" component={About} />
+              </Switch>
+            </div>
+          </Fragment>
+        </Router>
+      </TaskState>
+    </AuthState>
   );
-}
+};
 
 export default App;
