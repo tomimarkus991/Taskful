682d8da7391ed9507997dd2c31b76f4fd2ce2135
tomimarkus991 <tomimarkus.alber@tptlive.ee>
2020-05-15 17:15:09 +0300

    Added basic login and register form with states

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -1,7 +1,70 @@
-import React from "react";
+import React, { useState } from "react";
 
 const Register = () => {
-  return <div></div>;
+  const [user, setUser] = useState({
+    name: "",
+    email: "",
+    password: "",
+    confirmPassword: "",
+  });
+  const { name, email, password, confirmPassword } = user;
+
+  const onChange = (e) => setUser({ ...user, [e.target.name]: e.target.value });
+  const onSubmit = (e) => {
+    e.preventDefault();
+    console.log("Register Complete");
+    console.log(user);
+  };
+  return (
+    <div className="form-container">
+      <h1>
+        Account <span className="text-primary">Register</span>
+      </h1>
+      <form onSubmit={onSubmit}>
+        <div className="form-group">
+          <label htmlFor="name">Name</label>
+          <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={onChange}
+          ></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="email">Email Address</label>
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={onChange}
+          ></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="password">Password</label>
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={onChange}
+          ></input>
+        </div>
+        <div className="form-group">
+          <label htmlFor="confirmPassword">Confirm Password</label>
+          <input
+            type="password"
+            name="confirmPassword"
+            value={confirmPassword}
+            onChange={onChange}
+          ></input>
+        </div>
+        <input
+          type="submit"
+          value="Register"
+          className="btn btn-primary btn-block"
+        ></input>
+      </form>
+    </div>
+  );
 };
 
 export default Register;
--- client/src/components/layout/Navbar.js
+++ client/src/components/layout/Navbar.js
@@ -1,38 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { Link } from 'react-router-dom';
+import React from "react";
+import PropTypes from "prop-types";
+import { Link } from "react-router-dom";
 
 const Navbar = ({ title, icon }) => {
-    return (
-        <div className="navbar bg-primary">
-            <h1>
-                <ul>
-                    <li>
-                        <Link to="/">
-                            <i className={icon} /> {title}
-                        </Link>
-                    </li>
-                </ul>
-            </h1>
-            <ul>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/about">About</Link>
-                </li>
-            </ul>
-        </div>
-    )
-}
+  return (
+    <div className="navbar bg-primary">
+      <h1>
+        <ul>
+          <li>
+            <Link to="/">
+              <i className={icon} /> {title}
+            </Link>
+          </li>
+        </ul>
+      </h1>
+      <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/about">About</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+      </ul>
+    </div>
+  );
+};
 
 Navbar.propTypes = {
-    title: PropTypes.string.isRequired,
-    icon: PropTypes.string,
-}
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string,
+};
 
 Navbar.defaultProps = {
-    title: "Taskful",
-    icon: "fas fa-tasks"
-}
+  title: "Taskful",
+  icon: "fas fa-tasks",
+};
 export default Navbar;
